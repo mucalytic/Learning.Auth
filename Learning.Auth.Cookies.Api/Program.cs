0d580c94fe6d72aa706122989f1f5b13762478fa
mucalytic <mucalytic@hotmail.com>
2024-04-20 18:24:25 +0200

    Setting a cookie and retrieving it.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -2,6 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/username", (HttpContext context) =>
+{
+    var username =
+       (from cookie in context.Request.Headers["cookie"]
+        where cookie.StartsWith("auth=")
+        let parts = cookie.Split('=').Last()
+        where parts.StartsWith("usr:")
+        select parts.Split(':').Last())
+       .FirstOrDefault();
+    return username;
+});
+
+app.MapGet("/sign-in", (HttpContext context) =>
+{
+    context.Response.Headers["set-cookie"] = "auth=usr:aaron";
+    return "Ok";
+});
 
 app.Run();
