6765cff84a6b4f2b802ac3bdf2931f72faec12bf
mucalytic <mucalytic@hotmail.com>
2024-04-20 21:23:40 +0200

    Encoded the cookie.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -1,22 +1,30 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection();
 
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext context) =>
+app.MapGet("/username", (HttpContext context, IDataProtectionProvider provider) =>
 {
+    var protector = provider.CreateProtector("auth-cookie");
     var username =
        (from cookie in context.Request.Headers["cookie"]
         where cookie.StartsWith("auth=")
-        let parts = cookie.Split('=').Last()
+        let encoded = cookie.Split('=').Last()
+        let parts = protector.Unprotect(encoded)
         where parts.StartsWith("usr:")
         select parts.Split(':').Last())
        .FirstOrDefault();
     return username;
 });
 
-app.MapGet("/sign-in", (HttpContext context) =>
+app.MapGet("/sign-in", (HttpContext context, IDataProtectionProvider provider) =>
 {
-    context.Response.Headers["set-cookie"] = "auth=usr:aaron";
+    var protector = provider.CreateProtector("auth-cookie");
+    var cookie = $"auth={protector.Protect("usr:aaron")}";
+    context.Response.Headers["set-cookie"] = cookie;
     return "Ok";
 });
 
