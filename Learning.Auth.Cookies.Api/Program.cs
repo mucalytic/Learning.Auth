33478878e62eb65071577459df0abb2441a5b2f1
mucalytic <mucalytic@hotmail.com>
2024-04-20 21:53:02 +0200

    Created an auth service to sign user in and created some middleware to get the auth cookie and put its value in the context's user.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -1,30 +1,40 @@
 using Microsoft.AspNetCore.DataProtection;
+using Learning.Auth.Cookies.Api;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext context, IDataProtectionProvider provider) =>
+app.Use((context, next) =>
 {
+    var provider = context.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = provider.CreateProtector("auth-cookie");
-    var username =
-       (from cookie in context.Request.Headers["cookie"]
-        where cookie.StartsWith("auth=")
-        let encoded = cookie.Split('=').Last()
-        let parts = protector.Unprotect(encoded)
-        where parts.StartsWith("usr:")
-        select parts.Split(':').Last())
+    var (key, value) =
+        (from cookie in context.Request.Headers["cookie"]
+         where cookie.StartsWith("auth=")
+         let encoded = cookie.Split('=').Last()
+         let payload = protector.Unprotect(encoded)
+         where payload.StartsWith("usr:")
+         let parts = payload.Split(':').Take(2)
+         select (parts.First(), parts.Last()))
        .FirstOrDefault();
-    return username;
+    var claims = new List<Claim> { new(key, value) };
+    var identity = new ClaimsIdentity(claims);
+    context.User = new ClaimsPrincipal(identity);
+    return next();
 });
 
-app.MapGet("/sign-in", (HttpContext context, IDataProtectionProvider provider) =>
+app.MapGet("/username", (HttpContext context) =>
+    context.User.FindFirst("usr")?.Value);
+
+app.MapGet("/sign-in", (AuthService authService) =>
 {
-    var protector = provider.CreateProtector("auth-cookie");
-    var cookie = $"auth={protector.Protect("usr:aaron")}";
-    context.Response.Headers["set-cookie"] = cookie;
+    authService.SignIn();
     return "Ok";
 });
 
