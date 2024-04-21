23badf663b29d8b136bedb47dae3dffdbc0b9fd8
mucalytic <mucalytic@hotmail.com>
2024-04-21 16:57:25 +0200

    Only allowed to visit Russia if you have a Russian passport.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -18,12 +18,28 @@ app.MapGet("/sign-in", async (HttpContext context) =>
     var claims = new List<Claim>
     {
         new("usr", "aaron"),
-        new("passport", "uk")
+        new("passport", "uk"),
+        new("passport", "russia")
     };
     var identity = new ClaimsIdentity(claims, "cookie");
     var user = new ClaimsPrincipal(identity);
     await context.SignInAsync("cookie", user);
     return "Ok";
+});
+
+app.MapGet("/visiting/russia", (HttpContext context) =>
+{
+    if (context.User.Identities.All(i => i.AuthenticationType != "cookie"))
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return "not authenticated";
+    }
+    if (!context.User.HasClaim("passport", "russia"))
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        return "not authorised";
+    }
+    return "allowed";
 });
 
 app.Run();
