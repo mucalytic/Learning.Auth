534c0000d676488ff861965d88fa17ee994c0f2b
mucalytic <mucalytic@hotmail.com>
2024-04-21 17:12:37 +0200

    Authorisation now done in middleware.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -10,36 +10,39 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
-app.MapGet("/username", (HttpContext context) =>
-    context.User.FindFirst("usr")?.Value ?? "empty");
-
-app.MapGet("/sign-in", async (HttpContext context) =>
-{
-    var claims = new List<Claim>
-    {
-        new("usr", "aaron"),
-        new("passport", "uk"),
-        new("passport", "russia")
-    };
-    var identity = new ClaimsIdentity(claims, "cookie");
-    var user = new ClaimsPrincipal(identity);
-    await context.SignInAsync("cookie", user);
-    return "Ok";
-});
-
-app.MapGet("/visiting/russia", (HttpContext context) =>
+app.Use((context, next) =>
 {
     if (context.User.Identities.All(i => i.AuthenticationType != "cookie"))
     {
         context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-        return "not authenticated";
+        return Task.CompletedTask;
     }
-    if (!context.User.HasClaim("passport", "russia"))
+    if (!context.User.HasClaim("passport", "russia")) // the required claim comes from attribute or extension on the endpoint
     {
         context.Response.StatusCode = StatusCodes.Status403Forbidden;
-        return "not authorised";
+        return Task.CompletedTask;
     }
-    return "allowed";
+    return next();
 });
+
+app.MapGet("/username", (HttpContext context) =>
+    context.User.FindFirst("usr")?.Value ?? "empty");
+
+app.MapGet("/sign-in", async (HttpContext context) =>
+    {
+        var claims = new List<Claim>
+        {
+            new("usr", "aaron"),
+            new("passport", "uk"),
+            new("passport", "russia")
+        };
+        var identity = new ClaimsIdentity(claims, "cookie");
+        var user = new ClaimsPrincipal(identity);
+        await context.SignInAsync("cookie", user);
+        return "Ok";
+    })
+   .AllowAnonymous();
+
+app.MapGet("/visiting/russia", () => { });
 
 app.Run();
