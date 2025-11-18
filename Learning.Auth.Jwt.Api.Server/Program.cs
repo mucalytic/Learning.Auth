c29e3c2c169c8514c7e44232049e6628afa73609
mucalytic <mucalytic@hotmail.com>
2025-11-18 16:51:05 +0100

    Initial server implementation.

--- Learning.Auth.Jwt.Api.Server/Program.cs
+++ Learning.Auth.Jwt.Api.Server/Program.cs
@@ -1,14 +1,39 @@
+using System.Security.Claims;
 using System.Security.Cryptography;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 var rsaKey = RSA.Create();
 var privateKey = File.ReadAllBytes("key");
 rsaKey.ImportRSAPrivateKey(privateKey, out _);
 
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAuthentication("jwt")
+    .AddJwtBearer("jwt", options =>
+    {
+        
+    });
 
 var app = builder.Build();
 
 app.MapGet("/", (HttpContext ctx) => "hello world");
+app.MapGet("/jwt", () =>
+{
+    var key = new RsaSecurityKey(rsaKey);
+    var handler = new JsonWebTokenHandler();
+    var descriptor = new SecurityTokenDescriptor
+    {
+        Issuer = "https://localhost:5001/",
+        Subject = new ClaimsIdentity([
+            new Claim("sub", Guid.NewGuid().ToString()),
+            new Claim("name", "Aaron")
+        ]),
+        SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
+    };
+    var token = handler.CreateToken(descriptor);
+    return token;
+});
 
 app.Run();
