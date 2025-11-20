f384866f265a2a8c2b6960d99448129d154b3936
mucalytic <mucalytic@hotmail.com>
2025-11-20 15:21:07 +0100

    Added login endpoint and README.md file.

--- Learning.Auth.Basics.Api/Program.cs
+++ Learning.Auth.Basics.Api/Program.cs
@@ -4,9 +4,15 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/username", () =>
+app.MapGet("/username", () => // before we can get a username, we need to sign in. we need to issue a cookie.
 {
     return "aaron";
+});
+
+app.MapGet("/login", (HttpContext context) => // HttpContext contains all request headers, url, etc. throughout the lifetime of the request and response.
+{
+    context.Response.Headers["set-cookie"] = "auth=user:aaron";
+    return "ok";
 });
 
 app.Run();
---
+++ Learning.Auth.Basics.Api/README.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=ExQJljpj1lY
+
+Basics of authentication (with cookies).
