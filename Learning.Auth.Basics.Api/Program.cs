4f0b2849b389aa643465852d98dda90bd6aa1982
mucalytic <mucalytic@hotmail.com>
2025-11-21 12:37:09 +0100

    Implemented a NOT authentication.

--- Learning.Auth.Basics.Api/Program.cs
+++ Learning.Auth.Basics.Api/Program.cs
@@ -4,9 +4,12 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/username", () => // before we can get a username, we need to sign in. we need to issue a cookie.
+app.MapGet("/username", (HttpContext context) => // before we can get a username, we need to sign in. we need to issue a cookie.
 {
-    return "aaron";
+    var authCookie = context.Request.Headers.Cookie.FirstOrDefault(s => s is not null && s.StartsWith("auth="));
+    var payload = authCookie?.Split('=').LastOrDefault();
+    var username = payload?.Split(':').LastOrDefault();
+    return username ?? "empty";
 });
 
 app.MapGet("/login", (HttpContext context) => // HttpContext contains all request headers, url, etc. throughout the lifetime of the request and response.
