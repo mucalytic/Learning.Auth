3883d359fa2ddf7233a727b866a962603570a3a7
mucalytic <mucalytic@hotmail.com>
2025-11-21 12:55:13 +0100

    Using IDataProtectionProvider to encrypt the auth cookie payload.

--- Learning.Auth.Basics.Api/Program.cs
+++ Learning.Auth.Basics.Api/Program.cs
@@ -1,20 +1,28 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection(); // makes an IDataProtectionProvider available in DI
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/username", (HttpContext context) => // before we can get a username, we need to sign in. we need to issue a cookie.
+app.MapGet("/username", (HttpContext context, IDataProtectionProvider provider) => // before we can get a username, we need to sign in. we need to issue a cookie.
 {
+    var protector = provider.CreateProtector("auth-cookie");
     var authCookie = context.Request.Headers.Cookie.FirstOrDefault(s => s is not null && s.StartsWith("auth="));
-    var payload = authCookie?.Split('=').LastOrDefault();
-    var username = payload?.Split(':').LastOrDefault();
+    var protectedPayload = authCookie?.Split('=').LastOrDefault();
+    if (protectedPayload is null) return "empty";
+    var payload = protector.Unprotect(protectedPayload);
+    var username = payload.Split(':').LastOrDefault();
     return username ?? "empty";
 });
 
-app.MapGet("/login", (HttpContext context) => // HttpContext contains all request headers, url, etc. throughout the lifetime of the request and response.
+app.MapGet("/login", (HttpContext context, IDataProtectionProvider provider) => // HttpContext contains all request headers, url, etc. throughout the lifetime of the request and response.
 {
-    context.Response.Headers["set-cookie"] = "auth=user:aaron";
+    var protector = provider.CreateProtector("auth-cookie");
+    context.Response.Headers.SetCookie = $"auth={protector.Protect("user:aaron")}";
     return "ok";
 });
 
