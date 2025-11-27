ca5b562f72e7ca3df3ed22657f78b28ddf99b202
mucalytic <mucalytic@hotmail.com>
2025-11-27 18:45:19 +0100

    Initial implementation of basic api with authorisation.

--- Learning.Auth.Basics.Api.With.Authorisation/Program.cs
+++ Learning.Auth.Basics.Api.With.Authorisation/Program.cs
@@ -1,41 +1,28 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+const string authScheme = "cookie";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+       .AddAuthentication(authScheme)
+       .AddCookie(authScheme);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
-var summaries = new[]
+app.MapGet("/unsecure", (HttpContext context) =>
+    context.User.FindFirst("usr")?.Value ?? "empty");
+
+app.MapGet("/login", async (HttpContext context) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+    IEnumerable<Claim> claims = [new("usr", "aaron")];
+    var identity = new ClaimsIdentity(claims, authScheme);
+    var user = new ClaimsPrincipal(identity);
+    await context.SignInAsync(authScheme, user);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
