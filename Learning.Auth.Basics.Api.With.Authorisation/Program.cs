a112894ecf616ef9905c20a648af8d77db09869b
mucalytic <mucalytic@hotmail.com>
2025-11-27 19:18:31 +0100

    Implemented simple authorisation.

--- Learning.Auth.Basics.Api.With.Authorisation/Program.cs
+++ Learning.Auth.Basics.Api.With.Authorisation/Program.cs
@@ -17,9 +17,26 @@ app.UseAuthentication();
 app.MapGet("/unsecure", (HttpContext context) =>
     context.User.FindFirst("usr")?.Value ?? "empty");
 
-app.MapGet("/login", async (HttpContext context) =>
+app.MapGet("/switzerland", (HttpContext context) =>
 {
-    IEnumerable<Claim> claims = [new("usr", "aaron")];
+    // first must be authenticated with the correct schema ("cookie")
+    if (context.User.Identities.All(identity => identity.AuthenticationType != authScheme))
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return "unauthorised";
+    }
+    // then must have the correct claim with the correct value
+    if (context.User.HasClaim("pass", "ch")) return "allowed";
+    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+    return "not allowed";
+}); 
+
+app.MapGet("/login", async context =>
+{
+    IEnumerable<Claim> claims = [
+        new("usr", "aaron"),
+        new("pass", "ch")
+    ];
     var identity = new ClaimsIdentity(claims, authScheme);
     var user = new ClaimsPrincipal(identity);
     await context.SignInAsync(authScheme, user);
