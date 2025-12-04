ef75033c8891694d191197fc1aab8bf40b66e493
mucalytic <mucalytic@hotmail.com>
2025-12-04 18:18:04 +0100

    Added the same authorisation using the built-in authorisation implementation.

--- Learning.Auth.Basics.Api.With.Authorisation/Program.cs
+++ Learning.Auth.Basics.Api.With.Authorisation/Program.cs
@@ -9,41 +9,37 @@ builder.Services
        .AddAuthentication(authScheme)
        .AddCookie(authScheme);
 
+builder.Services
+       .AddAuthorization(options =>
+       {
+           options.AddPolicy("swiss-passport", policy =>
+           {
+               policy.RequireAuthenticatedUser()
+                     .AddAuthenticationSchemes(authScheme)
+                     .RequireClaim("pass", "ch");
+           });
+       });
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
-
-app.Use((context, next) =>
-{
-    if (context.Request.Path.StartsWithSegments("/login")) return next(context); // allow anonymous
-    if (context.User.Identities.All(identity => identity.AuthenticationType != authScheme))
-    {
-        context.Response.StatusCode = StatusCodes.Status401Unauthorized; // not authenticated
-        return Task.CompletedTask;
-    }
-    if (!context.User.HasClaim("pass", "ch"))
-    {
-        context.Response.StatusCode = StatusCodes.Status403Forbidden; // not authorised
-        return Task.CompletedTask;
-    }
-    return next(context);
-});
+app.UseAuthorization();
 
 // this will only return the name if you've logged in and have a "pass" claim with a value of "ch"
-app.MapGet("/unsecure", (HttpContext context) =>
-    context.User.FindFirst("usr")?.Value ?? "empty");
+app.MapGet("/unsecure", (HttpContext context) => context.User.FindFirst("usr")?.Value ?? "empty")
+   .RequireAuthorization("swiss-passport");
 
-app.MapGet("/login", async context =>
-{
-    IEnumerable<Claim> claims = [
-        new("usr", "aaron"),
-        new("pass", "ch")
-    ];
-    var identity = new ClaimsIdentity(claims, authScheme);
-    var user = new ClaimsPrincipal(identity);
-    // the user is specifically signing in with the schema "cookie"
-    await context.SignInAsync(authScheme, user);
-});
+app.MapGet("/login", async context => {
+        IEnumerable<Claim> claims = [
+            new("usr", "aaron"),
+            new("pass", "ch")
+        ];
+        var identity = new ClaimsIdentity(claims, authScheme);
+        var user = new ClaimsPrincipal(identity);
+        // the user is specifically signing in with the schema "cookie"
+        await context.SignInAsync(authScheme, user);
+    })
+   .AllowAnonymous();
 
 app.Run();
