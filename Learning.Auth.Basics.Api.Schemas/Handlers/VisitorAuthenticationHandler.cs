da7c21a19fcc3c3f310c79f748835c612d5e434d
mucalytic <mucalytic@hotmail.com>
2025-12-04 19:56:15 +0100

    Progress in creating authentication schemes.

---
+++ Learning.Auth.Basics.Api.Schemas/Handlers/VisitorAuthenticationHandler.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using System.Security.Claims;
+
+namespace Learning.Auth.Basics.Api.Schemas.Handlers;
+
+public class VisitorAuthenticationHandler(
+    IOptionsMonitor<CookieAuthenticationOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder) : CookieAuthenticationHandler(options, logger, encoder)
+{
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var result = await base.HandleAuthenticateAsync();
+        if (result.Succeeded) return result;
+        IEnumerable<Claim> claims = [new("usr", "aaron")];
+        var identity = new ClaimsIdentity(claims, "visitor");
+        var user = new ClaimsPrincipal(identity);
+        await Context.SignInAsync("visitor", user);
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
+}
--- Learning.Auth.Basics.Api.Schemas/Program.cs
+++ Learning.Auth.Basics.Api.Schemas/Program.cs
@@ -1,23 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Learning.Auth.Basics.Api.Schemas.Handlers;
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// https://youtu.be/N_zVCCpnjXM?si=ZdB4IQy6jxJps_aF
+
 builder.Services.AddAuthentication()
-                .AddCookie("visitor")
+                .AddScheme<CookieAuthenticationOptions, VisitorAuthenticationHandler>("visitor", _ => { })
                 .AddCookie("local");
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("customer", policy =>
+    {
+        policy.AddAuthenticationSchemes("local", "visitor")
+              .RequireAuthenticatedUser();
+    });
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/login-local", async context =>
-{
-    IEnumerable<Claim> claims = [new("usr", "aaron")];
-    var identity = new ClaimsIdentity(claims, "local");
-    var user = new ClaimsPrincipal(identity);
-    await context.SignInAsync("local", user);
-});
+app.MapGet("/", () => "hello")
+   .RequireAuthorization("customer");
+
+app.MapGet("/login-local", async context => {
+        IEnumerable<Claim> claims = [new("usr", "aaron")];
+        var identity = new ClaimsIdentity(claims, "local");
+        var user = new ClaimsPrincipal(identity);
+        await context.SignInAsync("local", user);
+    })
+   .AllowAnonymous();
 
 app.Run();
