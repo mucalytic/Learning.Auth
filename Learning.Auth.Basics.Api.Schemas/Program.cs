92e017d6ea808c8fbea29e41e0a93e682793e156
mucalytic <mucalytic@hotmail.com>
2025-12-05 20:06:51 +0100

    Added OAuth third-party authentication via wiremock.

--- Learning.Auth.Basics.Api.Schemas/Program.cs
+++ Learning.Auth.Basics.Api.Schemas/Program.cs
@@ -5,17 +5,36 @@ using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// video:
 // https://youtu.be/N_zVCCpnjXM?si=ZdB4IQy6jxJps_aF
 
 builder.Services.AddAuthentication()
-                .AddScheme<CookieAuthenticationOptions, VisitorAuthenticationHandler>("visitor", _ => { })
-                .AddCookie("local");
+                .AddScheme<CookieAuthenticationOptions, VisitorAuthenticationHandler>("visitor", _ => { }) // give a visitor a token no questions asked
+                .AddCookie("local")
+                .AddCookie("wiremock")
+                .AddOAuth("external-wiremock", options => // https://docs.wiremock.io/security/oauth2-mock
+                {
+                    options.SignInScheme = "wiremock";
+                    options.ClientId = "id"; // username
+                    options.ClientSecret = "secret"; // password
+                    options.AuthorizationEndpoint = "https://oauth.wiremockapi.cloud/oauth/authorize";
+                    options.TokenEndpoint = "https://oauth.wiremockapi.cloud/oauth/token";
+                    options.UserInformationEndpoint = "https://oauth.wiremockapi.cloud/userinfo";
+                    options.CallbackPath = "/callback-wiremock";
+                    options.Scope.Add("profile");
+                    options.SaveTokens = true;
+                });
 
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("customer", policy =>
     {
-        policy.AddAuthenticationSchemes("local", "visitor")
+        policy.AddAuthenticationSchemes("local", "wiremock", "visitor")
+              .RequireAuthenticatedUser();
+    });
+    options.AddPolicy("user", policy =>
+    {
+        policy.AddAuthenticationSchemes("local")
               .RequireAuthenticatedUser();
     });
 });
@@ -26,10 +45,7 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", (HttpContext context) =>
-    {
-        return Results.Ok("hello");
-    })
+app.MapGet("/", (HttpContext context) => Results.Ok("hello"))
    .RequireAuthorization("customer");
 
 app.MapGet("/login-local", async context => {
@@ -39,5 +55,8 @@ app.MapGet("/login-local", async context => {
         await context.SignInAsync("local", user);
     })
     .AllowAnonymous();
+
+app.MapGet("/login-wiremock", async context => await context.ChallengeAsync("external-wiremock", new AuthenticationProperties { RedirectUri = "/" }))
+   .RequireAuthorization("user"); // use the policy "user", which means we need to be authenticated with the "local" scheme NOT "visitor"
 
 app.Run();
