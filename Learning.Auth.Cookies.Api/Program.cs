f8db8434a8f56341d0d99cbcbef16ea5cdcff400
mucalytic <mucalytic@hotmail.com>
2025-12-08 16:51:11 +0100

    Exploring authentication options. Calling endpoints via fetch requests from JavaScript.

--- Learning.Auth.Cookies.Api/Program.cs
+++ Learning.Auth.Cookies.Api/Program.cs
@@ -1,16 +1,37 @@
 // https://youtu.be/hw2B6SZj8y8?si=tmi8-mft3bYkSgvQ&t=476
 
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication()
-                .AddCookie("default", options => options.Cookie.Name = "aaron.cookie");
+builder.Services.AddAuthentication("default") // set default scheme so that you don't have to specify authorisation policies
+                .AddCookie("default", options =>
+                {
+                    options.Cookie.Name = "aaron.cookie";
+                    options.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+                });
+builder.Services.AddAuthorization();
 builder.Services.AddControllers(); // allows you to pick up controllers from the file directory
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapGet("/", () => "hello");
+app.MapGet("/test", () => "hello")
+   .RequireAuthorization();
+app.MapPost("/login", async (HttpContext httpContext) =>
+{
+    IEnumerable<Claim> claims = [
+        new(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()) // userId
+    ];
+    var identity = new ClaimsIdentity(claims, "default"); // authentication type can be anything
+    var user = new ClaimsPrincipal(identity);
+    await httpContext.SignInAsync("default", user, new AuthenticationProperties { IsPersistent = true }); // must match the registered authentication (cookie) scheme
+    return "ok";
+});
 app.MapDefaultControllerRoute();
 app.Run();
