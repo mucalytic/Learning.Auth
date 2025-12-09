27a05e681bb47a499f59f6446cbfe5cb886583f2
mucalytic <mucalytic@hotmail.com>
2025-12-09 13:05:49 +0100

    SSO using Redis store to persist keys between API's.

--- Learning.Auth.Cookies.Sso.Api/Program.cs
+++ Learning.Auth.Cookies.Sso.Api/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection()
+                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("127.0.0.1:6379"))
+                .SetApplicationName("unique");
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGet("/", () => "hello");
+app.MapGet("/protected", () => "secret!")
+    .RequireAuthorization();
 app.Run();
--- Learning.Auth.Cookies.Sso.Identity/Program.cs
+++ Learning.Auth.Cookies.Sso.Identity/Program.cs
@@ -1,7 +1,34 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+using System.Security.Claims;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection()
+                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect("127.0.0.1:6379"))
+                .SetApplicationName("unique");
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGet("/", () => "hello");
+app.MapGet("/protected", () => "secret!")
+   .RequireAuthorization();
+app.MapGet("/login", async (HttpContext context) =>
+{
+    await context.SignInAsync(new ClaimsPrincipal([
+        new ClaimsIdentity([
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+        ],
+        CookieAuthenticationDefaults.AuthenticationScheme)
+    ]));
+    return "ok";
+});
 app.Run();
