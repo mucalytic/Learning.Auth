ff8c5c09f1ee108e66dccaa9a4964b870dbf502f
mucalytic <mucalytic@hotmail.com>
2026-01-22 18:56:31 +0100

    Added OAuth authentication and notes.md

--- Learning.Auth.OAuth.Api/Program.cs
+++ Learning.Auth.OAuth.Api/Program.cs
@@ -1,7 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication().AddOAuth("github", options =>
+{
+    options.ClientId = "Ov23liGvE0Z5ZMfIjjnL";
+    options.ClientSecret = "3eca3e78147ff5d9e11e9bbe31c76434d5a537e2";
+    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+    options.UserInformationEndpoint = "https://api.github.com/user";
+    options.CallbackPath = "/oauth/callback";
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello world!");
+app.UseAuthentication();
+
+app.MapGet("/login", () =>
+    Results.Challenge(authenticationSchemes: ["github"]));
+
+app.MapGet("/", (HttpContext context) =>
+    Results.Ok(context.User.Claims.Select(claim => new { claim.Type, claim.Value })));
 
 app.Run();
---
+++ Learning.Auth.OAuth.Api/notes.md
@@ -0,0 +1,11 @@
+Use GitHub as your authentication server.
+
+Setup:
+- Click on your user icon.
+- Click on Settings.
+- Click on Developer settings.
+- Click on OAuth Apps.
+- Click on New OAuth App.
+- Fill in the required fields.
+- Click on Register application.
+- Copy the Client ID and generate and copy the Client Secret.
