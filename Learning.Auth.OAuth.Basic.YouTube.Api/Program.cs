eb0d80adbe0ca2715ea1843145a60b995418ffaa
mucalytic <mucalytic@hotmail.com>
2026-01-25 10:43:31 +0100

    Simple cookie authentication.

--- Learning.Auth.OAuth.Basic.YouTube.Api/Program.cs
+++ Learning.Auth.OAuth.Basic.YouTube.Api/Program.cs
@@ -1,7 +1,17 @@
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("cookie").AddCookie("cookie");
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+
+app.MapGet("/login", () =>
+    Results.SignIn(
+        new ClaimsPrincipal(
+            new ClaimsIdentity([new Claim("user_id", Guid.NewGuid().ToString())], "cookie")),
+        authenticationScheme: "cookie"));
 
 app.Run();
