99b7030a7fd251c0c4e577b53b630b5e8080aa39
mucalytic <mucalytic@hotmail.com>
2026-01-25 12:42:54 +0100

    Making OAuth with YouTube happen upon access denied by adding a challenge. Included lots of notes in explanation.

--- Learning.Auth.OAuth.Basic.YouTube.Api/Program.cs
+++ Learning.Auth.OAuth.Basic.YouTube.Api/Program.cs
@@ -1,21 +1,42 @@
 using Learning.Auth.OAuth.Basic.YouTube.Api;
+using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<OAuthOptions>("youtube", builder.Configuration.GetSection("youtube"));
 builder.Services.AddAuthentication("cookie")
-                .AddCookie("cookie", options => options.LoginPath = "/login")
+                .AddCookie("cookie", options =>
+                {
+                    var action = options.Events.OnRedirectToAccessDenied;
+                    options.Events.OnRedirectToAccessDenied = context =>
+                    {
+                        /* 1. navigating to / checks "youtube-enabled" policy
+                         * 2. that requires authentication under the "cookie" scheme
+                         * 3. so it redirects to the /login endpoint which sets a cookie containing user details
+                         * 4. then it redirects back to / which again checks the "youtube-enabled" policy
+                         * 5. that policy also requires that the user has the "youtube-token" claim
+                         * 6. because they don't, so it redirects them to the access denied page
+                         * 7. we catch it here and use oath with youtube
+                         */
+                        if (context.Request.Path == "/")
+                        {
+                            return context.HttpContext.ChallengeAsync("youtube");
+                        }
+                        return action(context);
+                    };
+                    options.LoginPath = "/login";
+                })
                 .AddOAuth("youtube", options =>
-{
-    options.ClientId = builder.Configuration["youtube:clientId"] ?? string.Empty;
-    options.ClientSecret = builder.Configuration["youtube:clientSecret"] ?? string.Empty;
-    options.AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-    options.TokenEndpoint = "https://oauth2.googleapis.com/token";
-    options.CallbackPath = "/oauth/callback";
-    options.SignInScheme = "cookie";
-    options.SaveTokens = false;
-});
+                {
+                    options.ClientId = builder.Configuration["youtube:clientId"] ?? string.Empty;
+                    options.ClientSecret = builder.Configuration["youtube:clientSecret"] ?? string.Empty;
+                    options.AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+                    options.TokenEndpoint = "https://oauth2.googleapis.com/token";
+                    options.CallbackPath = "/oauth/callback";
+                    options.SignInScheme = "cookie";
+                    options.SaveTokens = false;
+                });
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("youtube-enabled", policy =>
