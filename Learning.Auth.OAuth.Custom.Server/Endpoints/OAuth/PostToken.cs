78280c169e138a159a94cbd149316cb30c6fdccc
mucalytic <mucalytic@hotmail.com>
2026-01-26 10:56:38 +0100

    Upgrading and adding nuget packages. Writing the Post token endpoint handler.

--- Learning.Auth.OAuth.Custom.Server/Endpoints/OAuth/PostToken.cs
+++ Learning.Auth.OAuth.Custom.Server/Endpoints/OAuth/PostToken.cs
@@ -1,6 +1,44 @@
+using System.Security.Claims;
+using Learning.Auth.OAuth.Custom.Server.Services;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace Learning.Auth.OAuth.Custom.Server.Endpoints.OAuth;
 
 public static class PostToken
 {
-    public static IResult Handler() => Results.Ok();
+    public static async Task<IResult> Handler(
+        HttpRequest request, DevKeysService devKeysService)
+    {
+        var bodyBytes = await request.BodyReader.ReadAsync();
+        var bodyContent = Encoding.UTF8.GetString(bodyBytes.Buffer);
+        string grantType = "", code = "", redirectUri = "", codeVerifier = "";
+        foreach (var part in bodyContent.Split('&'))
+        {
+            var keyValue = part.Split('=');
+            if (keyValue[0] == "grant_type") grantType = keyValue[1];
+            if (keyValue[0] == "code") code = keyValue[1];
+            if (keyValue[0] == "redirect_uri") redirectUri = keyValue[1];
+            if (keyValue[0] == "code_verifier") codeVerifier = keyValue[1];
+        }
+
+        var handler = new JsonWebTokenHandler();
+        return Results.Ok(new
+        {
+            access_token = handler.CreateToken(
+                new SecurityTokenDescriptor
+                {
+                    Claims = new Dictionary<string, object>
+                    {
+                        [JwtRegisteredClaimNames.Sub] = Guid.NewGuid().ToString(),
+                        ["custom"] = "foo"
+                    },
+                    Expires = DateTime.Now.AddMinutes(15),
+                    TokenType = "Bearer",
+                    SigningCredentials = new SigningCredentials(devKeysService.RsaSecurityKey, SecurityAlgorithms.RsaSha256)
+                }),
+            token_type = "Bearer"
+        });
+    }
 }
--- Learning.Auth.OAuth.Custom.Server/Program.cs
+++ Learning.Auth.OAuth.Custom.Server/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication("cookie")
                 .AddCookie("cookie", options => options.LoginPath = "/login");
-
 builder.Services.AddAuthorization();
 builder.Services.AddSingleton<DevKeysService>();
 
@@ -18,7 +17,6 @@ app.UseAuthorization();
 app.MapGet("/login",           GetLogin.Handler);
 app.MapPost("/login",          PostLogin.Handler);
 app.MapPost("/oauth/token",    PostToken.Handler);
-app.MapGet("/oauth/authorize", GetAuthorize.Handler)
-   .RequireAuthorization();
+app.MapGet("/oauth/authorize", GetAuthorize.Handler).RequireAuthorization();
 
 app.Run();
