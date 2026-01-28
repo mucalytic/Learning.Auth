8a56fb6705b92c7fcf7a2ec3ceaea44f41789abd
mucalytic <mucalytic@hotmail.com>
2026-01-28 10:57:24 +0100

    Changed the implementation of TokenDatabase to save to a file.

--- Learning.Auth.OAuth.Refresh.Tokens/Services/TokenDatabase.cs
+++ Learning.Auth.OAuth.Refresh.Tokens/Services/TokenDatabase.cs
@@ -1,20 +1,39 @@
 using Learning.Auth.OAuth.Refresh.Tokens.Interfaces;
 using Learning.Auth.OAuth.Refresh.Tokens.Entities;
+using System.Text.Json;
 
 namespace Learning.Auth.OAuth.Refresh.Tokens.Services;
 
-public class TokenDatabase : ITokenDatabase
+public class TokenDatabase(IWebHostEnvironment environment) : ITokenDatabase
 {
-    private readonly Dictionary<string, TokenInfo> _tokens = new();
-    
-    public Task<TokenInfo?> TryLoadAsync(string patreonId) =>
-        _tokens.TryGetValue(patreonId, out var tokenInfo)
-            ? Task.FromResult<TokenInfo?>(tokenInfo)
-            : Task.FromResult<TokenInfo?>(null);
+    private readonly string _path = Path.Combine(environment.ContentRootPath, "tokens.json");
+
+    public Task<TokenInfo?> TryLoadAsync(string patreonId)
+    {
+        if (File.Exists(_path))
+        {
+            var text = File.ReadAllText(_path);
+            var tokens = JsonSerializer.Deserialize<Dictionary<string, TokenInfo>>(text)
+                                             ?? new Dictionary<string, TokenInfo>();
+            return tokens.TryGetValue(patreonId, out var tokenInfo)
+                ? Task.FromResult<TokenInfo?>(tokenInfo)
+                : Task.FromResult<TokenInfo?>(null);
+        }
+        File.Create(_path).Close();
+        return Task.FromResult<TokenInfo?>(null);
+    }
 
     public Task<bool> TrySaveAsync(string patreonId, TokenInfo tokenInfo)
     {
-        _tokens[patreonId] = tokenInfo;
+        var tokens = new Dictionary<string, TokenInfo>();
+        if (File.Exists(_path))
+        {
+            var json = File.ReadAllText(_path);
+            tokens = JsonSerializer.Deserialize<Dictionary<string, TokenInfo>>(json)
+                                         ?? new Dictionary<string, TokenInfo>();
+            tokens[patreonId] = tokenInfo;
+        }
+        File.WriteAllText(_path, JsonSerializer.Serialize(tokens));
         return Task.FromResult(true);
     }
 }
